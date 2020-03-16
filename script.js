f9c76a40212bd9ddd9e10b5365b8514ac223ad23
Georgie <georginakellis@gmail.com>
2020-03-16 17:34:28 +0800

    prevent-default-timeblock-colours

--- script.js
+++ script.js
@@ -75,8 +75,49 @@ localStorageFunction();
 //function for prevent default
 
 $("button").on("click", function(event) {
-    
-})
+    event.preventDefault();
+    var textArea1 = $("textarea")[0].value;
+    var textArea2 = $("textarea")[1].value;
+    var textArea3 = $("textarea")[2].value;
+    var textArea4 = $("textarea")[3].value;
+    var textArea5 = $("textarea")[4].value;
+    var textArea6 = $("textarea")[5].value;
+    var textArea7 = $("textarea")[6].value;
+    var textArea8 = $("textarea")[7].value;
+    var textArea9 = $("textarea")[8].value;
+
+    localStorage.setItem("textarea1", textArea1);
+    localStorage.setItem("textarea2", textArea2);
+    localStorage.setItem("textarea3", textArea3);
+    localStorage.setItem("textarea4", textArea4);
+    localStorage.setItem("textarea5", textArea5);
+    localStorage.setItem("textarea6", textArea6);
+    localStorage.setItem("textarea7", textArea7);
+    localStorage.setItem("textarea8", textArea8);
+    localStorage.setItem("textarea9", textArea9);
+});
+
+
 
 //function for creating timeblock colours dependent on time - past, present, future
+
+function hourUpdater() {
+    var currentHour = moment().hours();
+    $(".time-block").each(function() {
+        var blockHour = parseInt($(this).attr("id").split(" ")[0]);
+
+        if (blockHour < currentHour) {
+            $(this).addClass("past");
+        } else if (blockHour === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        } else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+    });
+}
+
+hourUpdater();
 
