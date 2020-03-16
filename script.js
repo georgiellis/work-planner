db982b4b0622d72b24380d1e52fe1b95b0dbf82d
Georgie <georginakellis@gmail.com>
2020-03-16 10:18:35 +0800

    time-display-js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 
+var date = moment().format("dddd MMM Do YYYY");
+// var currentTime = moment().format('H');
+// console.log(currentTime);
+
+//-------append date----------//
+$("#currentDay").append(date);
+
